12a6b764967538ef42b35eee48b3f74f5369d214
Jasper <jmcclendon8383@gmail.com>
2026-05-16 21:51:44 -0500

    improve testing and continue porting process

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
 gcc main.c platform/platform-raylib.c -Wall -Wextra -o bin/mts26-0.1.0 -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+if [ $? -ne 0 ]; then
+    echo "Build failed"
+    exit 1
+fi
+echo "Build succeeded"
+exec bin/mts26-0.1.0
--- platform/platform-raylib.c
+++ platform/platform-raylib.c
@@ -3,15 +3,17 @@
 #include "stdio.h"
 #include "string.h"
 
+static Font serif;
 static Texture2D gameLogo;
 static Texture2D moneyTree;
 
 void onInit(void) {
     InitWindow(0,0,"Money Tree Simulator 2026");
+    serif = LoadFontEx("assets/PlayfairDisplay-MediumItalic.ttf",128,0,NULL);
     Image image = LoadImage("assets/logo.png");
     gameLogo = LoadTextureFromImage(image);
     UnloadImage(image);
-    image = LoadImage("assets/large/tree-money.png");
+    image = LoadImage("assets/desktop/tree-money.png");
     moneyTree = LoadTextureFromImage(image);
     UnloadImage(image);
     SetTargetFPS(60);
@@ -32,7 +34,8 @@ void onEndFrame(int state, int year, int money, bool harvest, int screenWidth, i
     ClearBackground((Color){44, 50, 25, 255});
     if (state == 0) {
         DrawTextureEx(moneyTree,(Vector2){screenWidth/2+parallaxX/2-moneyTree.width*1.05*0.5,screenHeight/2+parallaxY/2-moneyTree.height*1.05*0.5},0,1.05,WHITE);
-        DrawTextureEx(gameLogo,(Vector2){screenWidth/2+parallaxX-gameLogo.width*0.7*0.5,screenHeight/2+parallaxY-gameLogo.height*0.7*0.5},0,0.7,WHITE);
+        DrawTextureEx(gameLogo,(Vector2){screenWidth/2+parallaxX-gameLogo.width*0.7*0.5,screenHeight/2+parallaxY-gameLogo.height*0.7*0.5-40},0,0.7,WHITE);
+        DrawTextEx(serif,"Start New",(Vector2){screenWidth/2+parallaxX-50,screenHeight/2+parallaxY+100},64,1,WHITE);
     }
     char printBuffer[16];
     snprintf(printBuffer,sizeof(printBuffer),"%f mspf",frameTime);
